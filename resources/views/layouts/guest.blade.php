b8739cc9411833edc99db98d4c3c717059ad897b
CostaX549 <costasantos896@gmail.com>
2024-06-24 19:11:32 -0300

    Mudanças 24/06/2024 19:11

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,12 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>BarberConnect</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link rel="icon" href="{{ asset('barbearia.png') }}">
         <!-- Scripts -->
       <link rel="stylesheet" href="/build/assets/app-H3wy4wfz.css">
 
