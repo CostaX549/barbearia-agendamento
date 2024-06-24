f55af32649e71ae9366b33e994d397ea8b1947ac
CostaX549 <costasantos896@gmail.com>
2024-06-24 15:02:16 -0300

    24/06/2024 15:02

--- resources/views/livewire/cliente/barbearias/comentarios.blade.php
+++ resources/views/livewire/cliente/barbearias/comentarios.blade.php
@@ -1,242 +1,115 @@
-<div class=" flex flex-col  ">
-  <div class="w-[500px]  m-auto">
+<div x-data="avaliacaoData()">
+    <h2 class="mb-6 text-3xl text-center font-bold">
+        Comentários
+    </h2>
+    <div class="flex flex-wrap justify-center mb-6">
+        @foreach($this->avaliacoes as $avaliacao)
+        <div class="bg-white dark:bg-gray-800 text-black dark:text-gray-200 p-4 antialiased flex max-w-lg">
+            <img class="rounded-full h-8 w-8 mr-2 mt-1" src="{{ $avaliacao->user->profile_photo_url }}"/>
 
-  <div class="flex  justify-center items-center mb-2 ">
-    <div class="flex justify-center items-center  mb-2">
-      @for ($i = 1; $i <= 5; $i++)
-          @if($i <= floor($this->media))
-              <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-              </svg>
-          @elseif($i <= ceil($this->media) && $this->media != floor($this->media))
-              <svg class="w-4 h-4 text-yellow-300 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                  <defs>
-                      <linearGradient id="half-gradient">
-                          <stop offset="50%" stop-color="currentColor"/>
-                          <stop offset="50%" stop-color="gray"/>
-                      </linearGradient>
-                  </defs>
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" fill="url(#half-gradient)"/>
-              </svg>
-          @else
-              <svg class="w-4 h-4 text-gray-300 me-1 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-              </svg>
-          @endif
-      @endfor
-  </div>
-
-
-  <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">{{$this->media}}</p>
-  <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">de</p>
-  <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">5</p>
-
-
-
-  </div>
-
-  <p class="text-sm text-center  font-medium text-gray-500 dark:text-gray-400">{{$this->avaliacoes->count()}} Avaliações</p>
-
-
-
-
- <div class="w-[500px] m-auto p-5">
-
-
-@foreach($this->avaliacoes as $avaliacao)
-<article wire:key="{{ $avaliacao->id}}">
-  <div class="flex items-center mb-4 ">
-      <img class="w-10 h-10 me-4 rounded-full" src="{{ $avaliacao->user->profile_photo_url }}" alt="">
-      <div class="font-medium dark:text-white">
-          <p>{{ $avaliacao->user->name }} <time datetime="2014-08-16 19:00" class="block text-sm text-gray-500 dark:text-gray-400"></time></p>
-      </div>
-  </div>
-  <div class="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
-    <ul
-
-    wire:ignore
-
-    class="my-1 flex list-none gap-1 p-0"
-  >
-
-  @for ($i = 1; $i <= 5; $i++)
-
-  <li wire:ignore>
-      <span
-          class="text-primary [&>svg]:h-5 [&>svg]:w-5"
-          wire:ignore
-
-      >
-
-      @if ($i <= $avaliacao->qtd)
-      <svg xmlns="http://www.w3.org/2000/svg"
-      fill="blue"
-           viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-      </svg>
-      @else
-      <svg xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-      viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-     <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-  </svg>
-  @endif
-      </span>
-  </li>
-  @endfor
-  </ul>
-      <h3 class="ms-2 text-sm font-semibold text-gray-900 dark:text-white">Obrigado pela avaliação</h3>
-  </div>
-  <footer class="mb-5 text-sm text-gray-500 dark:text-gray-400"><p> <time datetime={{ $avaliacao->created_at }}>{{ $avaliacao->created_at }}</time></p></footer>
-  <p class="mb-2 text-gray-500 dark:text-gray-400">{{  $avaliacao->comment }}</p>
-
-
-  <aside>
-
-  </aside>
-</article>
-@endforeach
-
-
-
-
-  <div class="w-full px-3 mb-2 mt-6">
-    <ul  wire:ignore id="selected-value-example" class="ratingStar my-1 flex list-none gap-1 p-0">
-      <li>
-        <span
-          class="text-black [&>svg]:h-5 [&>svg]:w-5"
-          title="Bad"
-          data-te-rating-icon-ref>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-          </svg>
-        </span>
-      </li>
-      <li>
-        <span
-          class="text-black [&>svg]:h-5 [&>svg]:w-5"
-          title="Poor"
-          data-te-rating-icon-ref>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-          </svg>
-        </span>
-      </li>
-      <li>
-        <span
-          class="text-black [&>svg]:h-5 [&>svg]:w-5"
-          title="OK"
-          data-te-rating-icon-ref>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-          </svg>
-        </span>
-      </li>
-      <li>
-        <span
-          class="text-black [&>svg]:h-5 [&>svg]:w-5"
-          title="Good"
-          data-te-rating-icon-ref>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-          </svg>
-        </span>
-      </li>
-      <li>
-        <span
-          class="text-black [&>svg]:h-5 [&>svg]:w-5"
-          title="Excellent"
-          data-te-rating-icon-ref>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-          </svg>
-        </span>
-      </li>
-
-    </ul>
-
-      <textarea
-              class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-400 focus:outline-none focus:bg-white"
-              wire:model ="comment" placeholder="Avaliação" required></textarea>
-  </div>
-
-  <div class="w-full flex justify-end px-3 my-3">
-      <input type="submit"   id="botaoAvaliar"
-      data-barbearia-id="{{$barbearia->id}}"
-      type="button"
-    wire:loading.class="opacity-50"
-    wire:loading.attr="disabled"
-      class="inline-block  w-200 bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]" class="px-2.5 py-1.5 rounded-md text-white  bg-indigo-500 text-lg" value='Avalie a Barbearia'>
-  </div>
-  <script>
-    let valorAvaliado = 0; // Inicialize com um valor padrão ou 0
-const icons = document.querySelectorAll('#selected-value-example [data-te-rating-icon-ref]');
-
-icons.forEach((el) => {
-
-    el.addEventListener('onSelect.te.rating', (e) => {
-
-        valorAvaliado = e.value;
-    });
-});
-document.getElementById('botaoAvaliar').addEventListener('click', (e) => {
-  e.stopPropagation();
-var barbeariaId = document.getElementById('botaoAvaliar').getAttribute('data-barbearia-id');
-
-console.log('Barbearia ID:', barbeariaId);
-
-Livewire.dispatch('avaliar', {
-valor: valorAvaliado,
-id: barbeariaId,
-
-});
-
-});
-  </script>
-
-
-
-
-
-
+            <div>
+                <div class="bg-gray-100 dark:bg-gray-700 rounded-3xl px-4 pt-2 pb-2.5">
+                    <div class="font-semibold text-sm leading-relaxed">{{ $avaliacao->user->name }}</div>
+                    <div class="text-normal leading-snug md:leading-normal">
+                        {{ $avaliacao->comment }}
+                    </div>
+                </div>
+                <ul class="my-1 flex list-none gap-1 p-0">
+                    @for ($i = 1; $i <= 5; $i++)
+                    <li>
+                        <span class="text-primary [&>svg]:h-5 [&>svg]:w-5">
+                            @if ($i <= $avaliacao->qtd)
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="blue" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
+                            @else
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
+                            @endif
+                        </span>
+                    </li>
+                    @endfor
+                </ul>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    <div class=" w-[500px] m-auto px-3 mb-2 mt-6">
+        <ul id="selected-value-example" class="ratingStar my-1 flex list-none gap-1 p-0">
+            <template x-for="(rating, index) in ratings" :key="index">
+                <li>
+                    <span
+                        class="text-black [&>svg]:h-5 [&>svg]:w-5"
+                        :title="rating.title"
+                        data-te-rating-icon-ref
+                        @click="selectRating(index + 1)"
+                        x-bind:class="{'text-yellow-500': index < selectedRating}"
+                        @onSelect.te.rating="handleSelectRating(index + 1)">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                        </svg>
+                    </span>
+                </li>
+            </template>
+        </ul>
+        <textarea
+            class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-400 focus:outline-none focus:bg-white"
+            wire:model="comment"
+            placeholder="Avaliação"
+            required>
+        </textarea>
+    </div>
+    <div class="w-full flex justify-center mb-12 px-3 my-3">
+        <button
+            id="botaoAvaliar"
+            wire:loading.class="opacity-50"
+            :data-barbearia-id="{{ $barbearia->id }}"
+            @click="submitAvaliacao"
+            class="inline-block rounded-lg w-[450px] bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]">
+            Avalie a Barbearia
+        </button>
+    </div>
 </div>
+@script
+<script>
+
+        Alpine.data('avaliacaoData', () => ({
+            ratings: [
+                { title: 'Bad' },
+                { title: 'Poor' },
+                { title: 'Okay' },
+                { title: 'Good' },
+                { title: 'Excellent' }
+            ],
+            selectedRating: 0,
+
+            selectRating(value) {
+                this.selectedRating = value;
+                // Trigger custom event
+                this.$dispatch('onSelect.te.rating', { value: this.selectedRating });
+            },
+            handleSelectRating(value) {
+                this.selectedRating = value;
+            },
+            submitAvaliacao() {
+                let barbeariaId = document.getElementById('botaoAvaliar').getAttribute('data-barbearia-id');
+                console.log('Barbearia ID:', barbeariaId);
+                Livewire.dispatch('avaliar', {
+                    valor: this.selectedRating,
+                    id: barbeariaId,
+
+                });
+            }
+        }));
+
+</script>
+@endscript
--- resources/views/livewire/cliente/telas/barbearia-view.blade.php
+++ resources/views/livewire/cliente/telas/barbearia-view.blade.php
@@ -523,6 +523,9 @@ Galeria Completa
 
 
 
+<div id="comentario" x-intersect="shownComentario = true" x-intersect:leave="shownComentario = false">
+    <livewire:cliente.barbearias.comentarios :barbearia="$barbearia" :key="'comentarios-'.$barbearia->id" />
+</div>
 
 
 
@@ -605,35 +608,20 @@ Galeria Completa
     <div>
       <form action="">
         <div
-          class="gird-cols-1 grid items-center justify-center gap-4 md:grid-cols-3">
-          <div class="md:mb-6 md:ml-auto">
-            <p class="">
-              <strong>Sign up for our newsletter</strong>
-            </p>
-          </div>
+          class="m-auto">
+
 
           <!-- Newsletter sign-up input field -->
-          <div class="relative md:mb-6" data-te-input-wrapper-init>
-            <input
-              type="text"
-              class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] text-neutral-200 outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-              id="exampleFormControlInput1"
-              placeholder="Email address" />
-            <label
-              for="exampleFormControlInput1"
-              class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-200 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-neutral-200 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-              >Email address
-            </label>
-          </div>
+
 
           <!-- Newsletter sign-up submit button -->
           <div class="mb-6 md:mr-auto">
             <button
-              type="submit"
+              type="button"
               class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
               data-te-ripple-init
               data-te-ripple-color="light">
-              Subscribe
+             Inscreva-se
             </button>
           </div>
         </div>
@@ -642,91 +630,38 @@ Galeria Completa
 
     <!-- Copyright information -->
     <div class="mb-6">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-        distinctio earum repellat quaerat voluptatibus placeat nam,
-        commodi optio pariatur est quia magnam eum harum corrupti dicta,
-        aliquam sequi voluptate quas.
-      </p>
+        <p>
+            A Barbearia <strong>{{ $barbearia->nome }}</strong> é um verdadeiro oásis para quem busca não apenas um corte de cabelo ou barba impecáveis, mas também uma experiência de bem-estar e cuidado pessoal. Desde o momento em que você entra, é recebido pela atmosfera acolhedora e pelo ambiente cuidadosamente projetado, que combina estilo contemporâneo com um toque clássico.
+
+
+            Os profissionais altamente qualificados não apenas dominam as técnicas mais recentes de barbearia, mas também entendem a importância de um atendimento personalizado. Cada visita à Barbearia {{ $barbearia->nome }} é uma oportunidade de relaxar e desfrutar de serviços que vão além das expectativas, desde cortes de cabelo precisos até tratamentos de barba que deixam os clientes com uma aparência e sensação renovadas.
+
+
+            Além disso, o ambiente é complementado por uma seleção cuidadosa de produtos de alta qualidade, garantindo que cada serviço não só atenda, mas supere as expectativas dos clientes mais exigentes.
+        </p>
     </div>
 
     <!-- Links section -->
-    <div class="grid md:grid-cols-2 lg:grid-cols-4">
+    <div class="flex flex-col justify-center mt-2">
       <div class="mb-6">
-        <h5 class="mb-2.5 font-bold uppercase">Links</h5>
+        <h5 class="mb-2.5 font-bold uppercase">Serviços da Barbearia</h5>
 
-        <ul class="mb-0 list-none">
+        <ul class="mb-0 list-none ">
+
+          @forelse($barbearia->cortes as $corte)
           <li>
-            <a href="#!" class="text-white">Link 1</a>
+            <p  class="text-white">{{ $corte->nome }}</p>
           </li>
-          <li>
-            <a href="#!" class="text-white">Link 2</a>
+           @empty
+           <li>
+            <p  class="text-white">Nenhum serviço encontrado.</p>
           </li>
-          <li>
-            <a href="#!" class="text-white">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 4</a>
-          </li>
+          @endforelse
+
         </ul>
       </div>
 
-      <div class="mb-6">
-        <h5 class="mb-2.5 font-bold uppercase">Links</h5>
 
-        <ul class="mb-0 list-none">
-          <li>
-            <a href="#!" class="text-white">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 4</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="mb-6">
-        <h5 class="mb-2.5 font-bold uppercase">Links</h5>
-
-        <ul class="mb-0 list-none">
-          <li>
-            <a href="#!" class="text-white">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 4</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="mb-6">
-        <h5 class="mb-2.5 font-bold uppercase">Links</h5>
-
-        <ul class="mb-0 list-none">
-          <li>
-            <a href="#!" class="text-white">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" class="text-white">Link 4</a>
-          </li>
-        </ul>
-      </div>
     </div>
 
 
@@ -734,7 +669,7 @@ Galeria Completa
   <div
     class="w-full p-4 text-center"
    ">
-    © 2023 Copyright:
+    © 2024 Copyright:
     <a class="text-white" href="https://tw-elements.com/">BarberConnect</a>
   </div>
 </footer>
