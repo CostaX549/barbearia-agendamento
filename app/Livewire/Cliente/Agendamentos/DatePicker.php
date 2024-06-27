2ce9e652c67ce2b571198458ce52f837f63814d9
CostaX549 <costasantos896@gmail.com>
2024-06-27 11:30:00 -0300

    Mudanças 27/06/2024 11:29

--- app/Livewire/Cliente/Agendamentos/DatePicker.php
+++ app/Livewire/Cliente/Agendamentos/DatePicker.php
@@ -12,44 +12,54 @@ use App\Models\BarbeariaUser;
 
 class DatePicker extends Component
 {
-    #[Modelable] 
-    
+    #[Modelable]
+
     public string $date = '';
 
     public string $dateChanged = '';
     public ?BarbeariaUser $barbeiroSelecionado;
     public  ?Agendamento $selectedAgendamento = null;
-    
-    public  array $formattedDates = []; 
+
+    public  array $formattedDates = [];
 
     public function mount() {
         if($this->selectedAgendamento) {
-        
 
-      
-    
-         
+
+
+
+
             $datetime = Carbon::parse($this->date);
-   
-          
 
-        
-      
-     
-    
-        
+
+
+
+
+
+
+
+            $this->dateChanged = $datetime->format('d-m-Y');
+        } else {
+            $this->date = Carbon::now()->format('d-m-Y H:i');
+            $datetime = Carbon::parse($this->date);
             $this->dateChanged = $datetime->format('d-m-Y');
         }
     }
+
+    public function setDate($time)
+    {
+       $timeFormatted = Carbon::parse($time)->format('H:i');
+        $this->date = Carbon::parse($this->dateChanged)->setTimeFromTimeString($timeFormatted);
+    }
     public function updatedDate($value)
     {
-      
+
         $datetime = Carbon::parse($value);
-    
-    
-    
-       
-     
+
+
+
+
+
 
         $this->updateDateChanged($datetime);
 
--- resources/views/livewire/cliente/agendamentos/date-picker.blade.php
+++ resources/views/livewire/cliente/agendamentos/date-picker.blade.php
@@ -2,7 +2,7 @@
 
 <div >
 
- 
+
     <div  x-data="bob" x-init="initDatePicker($refs.datepicker2)"  wire:ignore>
         <div class="mb-4">
             <x-input type="text"  x-ref="datepicker2" wire:model.live="date" label="Data" placeholder="Selecione uma data" />
@@ -11,37 +11,37 @@
 
 
 
-    
-      
+
+
       <div class="ml-2" wire:loading wire:target="updateDateChanged">
         <div class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
             <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
         </div>
       </div>
-      
+
       @if($dateChanged)
       <p class="text-neutral-800  mb-2">
       Horários Disponíveis:
       </p>
       @foreach ($this->barbeiroSelecionado->getAllAvailableTimes($this->date, $this->selectedAgendamento) as $time)
-   
+
     @php
-        
+
         $color = $time['color'];
     @endphp
     @if ($color === 'red')
         <x-badge label="{{ $time['time']->format('H:i') }}" negative />
     @elseif($color === '')
-    <x-badge label="{{ $time['time']->format('H:i') }}"  />
-    @else 
-    <x-badge label="{{ $time['time']->format('H:i') }}" black />
+    <x-badge wire:click="setDate({{ $time['time'] }})" label="{{ $time['time']->format('H:i') }}"  />
+    @else
+    <x-badge  label="{{ $time['time']->format('H:i') }}" black />
     @endif
 @endforeach
    @endif
 
- 
+
 </div>
-@assets 
+@assets
 <script  src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
 <script  src="https://npmcdn.com/flatpickr/dist/l10n/pt.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/plugins/minMaxTimePlugin.js" integrity="sha512-zgiWQuiK570MGitC+mVHUDLx3irm+SJgFIZRvt76V0V/7z1Ta7eyKvrYqwb7zinesTxnVwoxvpWf4tKtNyHFvA==" crossorigin="anonymous" referrerpolicy="no-referrer" defer></script>
@@ -51,7 +51,7 @@
 <script>
   Alpine.data('bob', () => ({
       date: '',
-      
+
       initDatePicker(datepickerRef) {
           var enableDays = {!! json_encode($this->barbeiroSelecionado->workingHours->pluck('day_of_week')->toArray())  !!};
           var addDays = {!! $this->barbeiroSelecionado->specificDates->where("status", "adicionar")->pluck('start_date')->map(function($date) {
@@ -64,10 +64,10 @@ var workingHours = {!! json_encode(
     $this->barbeiroSelecionado->workingHours->map(function($workingHour) {
         $startHour = \Carbon\Carbon::parse($workingHour->start_hour);
         $endHour = \Carbon\Carbon::parse($workingHour->end_hour);
-        
+
         // Separar horas, minutos e segundos do intervalo
         $intervalParts = explode(':', $this->barbeiroSelecionado->interval);
-        
+
         // Verifica se o intervalo tem todas as partes necessárias
         if (count($intervalParts) === 3) {
             // Adiciona horas, minutos e segundos separadamente
@@ -79,9 +79,9 @@ var workingHours = {!! json_encode(
             // Aqui, apenas definimos $endHour para null
             $endHour = null;
         }
-        
+
         return [
-            'day' => $workingHour->day_of_week->value,  
+            'day' => $workingHour->day_of_week->value,
             'minTime' => $workingHour->start_hour,
             'maxTime' => $endHour ? $endHour->format('H:i') : null,
         ];
@@ -91,16 +91,16 @@ var removeDays = {!! $this->barbeiroSelecionado->specificDates->where("status", 
 return \Carbon\Carbon::parse($date)->format('d-m-Y');
 })->toJson() !!};
 
-   
-    
+
+
 
 var today = new Date();
 
 // Adicione 1 mês à data de hoje
 var nextMonth = new Date(today);
 nextMonth.setMonth(nextMonth.getMonth() + 1);
-       
-          
+
+
           var plugins = [];
 
 
@@ -110,23 +110,23 @@ nextMonth.setMonth(nextMonth.getMonth() + 1);
               inline: true,
               defaultDate: @json($date),
               maxDate: maxDate,
-              
-          
+
+
               minuteIncrement: 30,
               locale: 'pt',
-      
+
               minDate: 'today',
-       
+
 
 
                enable: [
   function (date) {
       var date = new Date(date);
       var formattedDate = ("0" + date.getDate()).slice(-2) + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + date.getFullYear();
-        
-     
-   
-  
+
+
+
+
       if (addDays.includes(formattedDate)) {
           return true;
       }
@@ -134,42 +134,42 @@ nextMonth.setMonth(nextMonth.getMonth() + 1);
       // Verifica se a data está em enableDays (dias permitidos)
       return enableDays.includes(date.getDay());
   }
-],      
+],
 
 
 onChange: function (selectedDates, dateStr, instance) {
-            
- 
+
+
             var selectedDate = selectedDates[0];
             var dayOfWeek = selectedDate.getDay();
-        
-   
+
+
         /*     if (!addDays.includes(dateStr.split(' ')[0])) {
                 var selectedWorkingHours = workingHours.find(function (hour) {
                     return hour.day === dayOfWeek;
                 });
-            
+
                 if (selectedWorkingHours) {
-               
+
                     instance.set('minTime', selectedWorkingHours.minTime);
-             
+
                     instance.set('maxTime', selectedWorkingHours.maxTime);
                 }
             } else {
-                instance.set('minTime', null); 
+                instance.set('minTime', null);
                 instance.set('maxTime', null);
             } */
-            
-        
-            
+
+
+
         }
 
 
-  
 
 
-              
-     
+
+
+
           });
       }
   }));
