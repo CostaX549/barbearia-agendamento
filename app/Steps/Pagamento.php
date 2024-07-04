97940c753021e1f1de519145090852788e8c782f
CostaX549 <costasantos896@gmail.com>
2024-07-03 22:17:43 -0300

    03/07/2024 22:11

--- app/Steps/Pagamento.php
+++ app/Steps/Pagamento.php
@@ -9,7 +9,7 @@ use Vildanbina\LivewireWizard\Components\Step;
 use Illuminate\Validation\Rule;
 use App\Models\Barbearia;
 use App\Models\Barbeiros;
-use Carbon\Carbon;  
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use App\Models\UserWorkingHours;
 use App\Models\Plan;
@@ -31,20 +31,20 @@ use App\Models\Maquininha;
 
 class Pagamento extends Step
 {
-    // Step view located at resources/views/steps/general.blade.php 
+    // Step view located at resources/views/steps/general.blade.php
     protected string $view = 'steps.pagamento';
     public $qrCode;
     public $cardIds = [];
 
- 
+
 
     /*
      * Initialize step fields
      */
     public function mount()
     {
-       
-   
+
+
     }
 
 
@@ -54,13 +54,13 @@ class Pagamento extends Step
     public function save($state, $formData, $paymentMethod,$selectedPlan)
     {
 
-          
-             
-        $accessToken = 'APP_USR-3577992641079180-011721-ff207db72804f196d2066d2931ed850c-1644143944'; 
+
+
+        $accessToken = 'APP_USR-3577992641079180-011721-ff207db72804f196d2066d2931ed850c-1644143944';
 
 
         MercadoPagoConfig::setAccessToken($accessToken);
-      
+
 
         $barbearia = new Barbearia;
         $barbearia->nome = $state['name'];
@@ -70,31 +70,31 @@ class Pagamento extends Step
         $barbearia->rua = $state['rua'];
         $barbearia->cidade = $state['cidade'];
 
-       
-        
+
+
         $barbearia_user = new BarbeariaUser;
         $barbearia_user->payment_methods_allowed = $state['payments'];
 
-        
+
         $barbearia_user->user_id = auth()->user()->id;
-    
-     
+
+
         if (defined(PaymentMethods::class . '::' . $formData['payment_method_id'])) {
-   
+
         $barbearia_user->payment_method = constant(PaymentMethods::class . '::' . $formData['payment_method_id']);
-          
+
         } else {
-       
+
 
             $barbearia_user->payment_method = constant(PaymentMethods::class . '::' . $paymentMethod);
         }
         $barbearia_user->price = constant(PlanTypes::class . '::' . 'mensal');
         $barbearia->estado = $state['estado'];
-        $barbearia->complemento = $state['complemento'];      
+        $barbearia->complemento = $state['complemento'];
         $barbearia->owner_id = auth()->user()->id;
         $barbearia->slug = $state['slug'];
         $barbearia->cpf = $state['cpf'];
-     
+
         $barbearia->save();
         $barbearia_user->barbearia_id = $barbearia->id;
         $barbearia_user->save();
@@ -106,28 +106,28 @@ class Pagamento extends Step
         $maquininha->save();
         $barbearia_user->delete();
 
-        
-       
-        
 
-        $dia = null; 
+
+
+
+        $dia = null;
 
         foreach ($state['dias'] as $index => $ativo) {
             if ($ativo) {
 
                 $indexInt = $index;
-        
-               
+
+
                 $dia = DaysOfWeek::from($indexInt);
 
- 
+
 
                 if ($dia !== null) {
                     UserWorkingHours::create([
-                  
-                      
+
+
                  'barbearia_user_id' => $barbearia_user->id,
-                        'day_of_week' => $dia->value, 
+                        'day_of_week' => $dia->value,
                         'start_hour' => $state['horariosIniciais'][$index],
                         'end_hour' => $state['horariosFinais'][$index],
                         'intervals' => [
@@ -140,38 +140,38 @@ class Pagamento extends Step
                 }
             }
         }
-  
+
 if($paymentMethod === 'debit_card' || $paymentMethod === 'credit_card' ) {
 
-  
+
 
 
     $client = new PreApprovalClient();
-  
+
 
     $preapprovalData = [
 
-        
-        
-        
-     
-   'preapproval_plan_id' => '2c9380848fa6c953018fb793b05702e7',
 
-      
-        
+
+
+
+   'preapproval_plan_id' => '2c9380849075726001907b4af1280234',
+
+
+
          'payer_email'=> $formData['payer']['email'],
-          
+
           'card_token_id' => $formData['token'],
-        
+
 
         'external_reference' => $barbearia_user->id
-      
-    
+
+
 
     ];
 
 
-        
+
 
     try {
         $assinatura = $client->create($preapprovalData);
@@ -195,11 +195,11 @@ if($paymentMethod === 'debit_card' || $paymentMethod === 'credit_card' ) {
         $client_customer = new CustomerClient();
         $customer = $client_customer->create(["email" => auth()->user()->email]);
 
-    
+
         auth()->user()->payer_id = $customer->id;
         auth()->user()->save();
     } else {
-       
+
         auth()->user()->payer_id = $customer->results[0]->id;
         auth()->user()->save();
     }
@@ -209,16 +209,16 @@ if($paymentMethod === 'debit_card' || $paymentMethod === 'credit_card' ) {
    $response = $client_card->create(auth()->user()->payer_id, ["token" => $formData['token']]);
 } catch (\Exception $e) {
     dd($e);
-}   
+}
      $barbearia_user->card_id = $response->id;
     $barbearia_user->save();
  Redirect::route('barbearia.billing', ['slug' => $barbearia->slug]);
-   
-  
 
 
-   
-    
+
+
+
+
 
 } else {
     $preferenceID = '1644143944-a0ead566-60cf-4dd3-a40d-a57dc92ba2a3';
@@ -227,25 +227,25 @@ if($paymentMethod === 'debit_card' || $paymentMethod === 'credit_card' ) {
         'Authorization' => 'Bearer ' . $accessToken,
         'Content-Type' => 'application/json',
     ])->get("https://api.mercadopago.com/checkout/preferences/{$preferenceID}");
-    
-    
+
+
     $preco = $response->json();
-     
- 
+
+
     $idempotencyKey = uniqid();
-    
+
     $paymentData = [
          'transaction_amount'=> $preco['items'][0]['unit_price'] ,
-       
+
         'description' => 'Pague o plano do barbeiro',
 
         'payer' => [
-           
-            'email' => $formData['payer']['email'], 
-          
+
+            'email' => $formData['payer']['email'],
+
         ],
 
-    
+
 
         'external_reference' => $barbearia_user->id
     ];
@@ -260,20 +260,20 @@ if($paymentMethod === 'debit_card' || $paymentMethod === 'credit_card' ) {
     $customer = json_decode($customerResponse->body());
     $client_customer = new CustomerClient();
     if (empty($customer->results)) {
-       
+
         $customer = $client_customer->create(["email" => auth()->user()->email]);
-       
-    
+
+
         auth()->user()->payer_id = $customer->id;
         auth()->user()->save();
     } else {
-       
+
         auth()->user()->payer_id = $customer->results[0]->id;
         auth()->user()->save();
     }
- 
- 
-   
+
+
+
     if ($formData['payment_method_id'] === 'bolbradesco') {
 
         $paymentData['payer']['identification']['type'] = $formData['payer']['identification']['type'];
@@ -289,10 +289,10 @@ $paymentData['payer']['last_name'] = $formData['payer']['last_name'];
            $paymentData['payment_method_id'] = 'bolbradesco';
 
             $customer = $client_customer->update(auth()->user()->payer_id, [
-         
+
             "first_name" => $formData['payer']['first_name'],
             "last_name" => $formData['payer']['last_name'],
-           
+
             "identification" => array(
               "type" => "CPF",
               "number" => "12345678909"
@@ -304,10 +304,10 @@ $paymentData['payer']['last_name'] = $formData['payer']['last_name'];
               "street_name" => $formData['payer']['address']['street_name'],
               "street_number" => $formData['payer']['address']['street_name']
             )
-          ]); 
+          ]);
 
 
-      
+
     } elseif($formData['payment_method_id'] === 'pec') {
         $paymentData['payer']['identification']['type'] = 'CPF';
         $paymentData['payer']['first_name'] = $formData['payer']['first_name'];
@@ -315,75 +315,75 @@ $paymentData['payer']['last_name'] = $formData['payer']['last_name'];
            $paymentData['payer']['identification']['number'] = '12345678909';
            $paymentData['payment_method_id'] = 'pec';
     } else {
-      
+
         $paymentData['payment_method_id'] = 'pix';
-      
+
     }
-   
+
         $response = Http::withHeaders([
             'Authorization' => 'Bearer ' . $accessToken,
             'Content-Type' => 'application/json',
             'X-Idempotency-Key' => $idempotencyKey,
         ])->post("https://api.mercadopago.com/v1/payments?access_token={$accessToken}&preference_id={$preferenceID}", $paymentData);
- 
- 
 
-    
+
+
+
     $barbearia_user->payment_id = $response->json()['id'];
     $barbearia_user->plan_ends_at = $response->json()['date_of_expiration'];
-   
+
    $barbearia_user->save();
 
    Redirect::route('barbearia.billing', ['slug' => $barbearia->slug]);
-    
+
 }
-    
-
-    
- 
-
-   
 
 
 
-    
 
-      
+
+
+
+
+
+
+
+
 if($paymentMethod === 'debit_card' || $paymentMethod === 'credit_card' ) {
     $barbearia_user->assinatura_id = $assinatura->id;
-  
+
   $barbearia_user->plan_ends_at = Carbon::parse($assinatura->next_payment_date);
 
   $barbearia_user->save();
-  
-} 
 
-
-   
+}
 
 
 
 
 
- 
-      
-   
-
-    }   
 
 
-  
+
+
+
+
+
+    }
+
+
+
     private function limparCacheBarbearias()
     {
-  
+
         $cacheKey = "homepage-barbearias";
-    
-   
+
+
         Cache::forget($cacheKey);
     }
-    
+
     /*
-    * Step icon 
+    * Step icon
     */
     public function icon(): string
     {
@@ -392,7 +392,7 @@ if($paymentMethod === 'debit_card' || $paymentMethod === 'credit_card' ) {
 
 
 
- 
+
 
 
 
