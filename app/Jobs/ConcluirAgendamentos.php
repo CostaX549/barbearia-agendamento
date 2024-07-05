267cb9fec99aa7393aa733edc8dd1475aa4c7a6e
CostaX549 <costasantos896@gmail.com>
2024-07-05 10:35:01 -0300

    Mudanças 05/07/2024 11:34

--- app/Jobs/ConcluirAgendamentos.php
+++ app/Jobs/ConcluirAgendamentos.php
@@ -34,31 +34,31 @@ class ConcluirAgendamentos implements ShouldQueue
         $agendamentos = Agendamento::where('end_date', '<=', now())->get();
 
         foreach($agendamentos as $agendamento) {
-         
-              
+
+
     if ($agendamento->payment_method == 'Cartão de Crédito' && isset($agendamento->maquininha->taxa_credito)) {
         $agendamento->fatura_price = $agendamento->total_price - ($agendamento->maquininha->taxa_credito/100 * $agendamento->total_price);
-       
+
     } elseif($agendamento->payment_method == 'Cartão de Débito' && isset($agendamento->maquininha->taxa_debito)) {
         $agendamento->fatura_price = $agendamento->total_price - ($agendamento->maquininha->taxa_debito/100 * $agendamento->total_price);
     } else {
        $agendamento->fatura_price = $agendamento->total_price;
     }
-             
+
                $agendamento->save();
                $cliente = Cliente::where('user_id', $agendamento->owner_id)->first();
                if(!$cliente) {
                  $cliente = new Cliente();
                  $cliente->user_id = $agendamento->owner_id;
-                 $cliente->barbearia_id = $agendamento->barbeiros()->first()->barbearia->id;
+                 $cliente->barbearia_id = $agendamento->colaborador->barbearia->id;
                   $cliente->save();
                }
-         
-          
+
+
 
             $agendamento->delete();
 
-            
+
         }
     }
 }
