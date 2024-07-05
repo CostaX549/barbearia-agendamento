39f0544bbdf7451d25166a48c76dcab91b3895c0
CostaX549 <costasantos896@gmail.com>
2024-07-05 11:25:23 -0300

    Ai julio 11:264

--- resources/views/livewire/cliente/telas/teste.blade.php
+++ resources/views/livewire/cliente/telas/teste.blade.php
@@ -439,13 +439,13 @@ data-te-navbar-ref>
                 </li>
             </template>
         </ul>
-       
+
     </div>
     <div class="w-full flex justify-center mb-12 px-3 my-3">
         <button
             id="botaoAvaliar"
             wire:loading.class="opacity-50"
-            :data-barbearia-id="{{ $barbearia->id }}"
+            :data-barbearia-id="{{ $b->id }}"
             @click="submitAvaliacao"
             class="inline-block rounded-lg w-[450px] bg-neutral-800 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]">
             Avalie a Barbearia
